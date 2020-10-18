9c605c8db581090c9888bf1d06d2befacd803c34
AykutSarac <aykutsarac0@gmail.com>
2020-10-18 16:23:03 +0300

    wikia changes

--- example/example.js
+++ example/example.js
@@ -1,32 +1,5 @@
-const growapi = require('../index');
+const growapi = require('../lib/index');
 
-growapi.itemInfo("Golden Halo").then(data => {
-
-    /*
-     Item name has to be exact name and is case sensitive.
-     Returns json of item information including sprite link, recipe, description & properties.
-    */
-
-    console.log(data);
-
-}).catch(err => {
-    console.error(err);
+growapi.itemInfo("Angel Wings").then(result => {
+    console.log(result);
 });
-
-growapi.search("ancestral").then(data => {
-
-    console.log(data); // Returns a json with matchin item names.
-
-}).catch(err => {
-    console.error(err);
-});
-
-
-growapi.server().then(data => {
-    console.log(data);
-}).catch(err => {
-    console.error(err);
-})
-
-
-

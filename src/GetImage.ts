4e4e51ae0623810ff0cc4795c0099be975e399f1
Aykut Saraç <aykutsarac0@gmail.com>
2022-03-29 12:24:06 +0300

    fix webp -> png

--- src/GetImage.ts
+++ src/GetImage.ts
@@ -10,13 +10,13 @@ async function getImage(input: string): Promise<string | undefined> {
         const itemName = itemList[0].title;
         const link = `https://growtopia.wikia.com/wiki/${itemName}`;
 
-        const getData = await axios.get(link).then(res => res.data);
+        const getData = (await axios.get(link)).data;
         const $ = cheerio.load(getData);
 
-        const Sprite = $("div.card-header .growsprite > img").attr('src');
+        const Sprite = $("div.card-header .growsprite > img").attr('src');  
         if (!Sprite) return undefined;
 
-        return Sprite;
+        return Sprite.replace('webp', 'png');
     } catch (error) {
         if (axios.isAxiosError(error)) {
             throw error?.response;
